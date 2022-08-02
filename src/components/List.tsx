1d81d1c91e3ca2113dd3618c016b5983f8e60403
Alex Tomas <atomasllimos@gmail.com>
2022-08-02 19:47:55 +0200

    Using renderList with type JSX.Element[]

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -6,19 +6,25 @@ interface Props {
         description?: string
     }>
 }
-const List: React.FunctionComponent<Props> = ({subs}) =>  {
+const List = ({subs}: Props) =>  {
+
+    const renderList = (): JSX.Element[] => {
+        return (
+            subs.map(sub => {
+                return (
+                  <li key={sub.nick}>
+                    <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+                    <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+                    <p>{sub.description?.substring(0, 100)}</p>
+                  </li>
+                )
+              })
+        )
+    }
     return (
         <ul>
         {
-          subs.map(sub => {
-            return (
-              <li key={sub.nick}>
-                <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
-                <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
-                <p>{sub.description?.substring(0, 100)}</p>
-              </li>
-            )
-          })
+          renderList()
         }
         </ul>
     )
